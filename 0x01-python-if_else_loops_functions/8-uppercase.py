578e30065daf5571e4423fdcfc8d5707e36a6a33
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-06-06 10:37:25 -0700

    code for task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
         ascii_value = ord(char)
         if 97 <= ascii_value <= 122:
             upper_char = chr(ascii_value - 32)
-            print(upper_char, end='')
+            print("{}".format(upper_char), end='')
     print()
