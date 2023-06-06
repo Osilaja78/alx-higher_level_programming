9984f194bec52aff25b13fd8d93c456c82ab5367
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-06-06 11:02:04 -0700

    code for task 11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def pow(a, b):
+    a = int(a)
+    b = int(b)
+
+    return (a ** b)
+
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
