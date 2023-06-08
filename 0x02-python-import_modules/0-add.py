37d0fd76f68e2de0fdf7cc80c126717b75bd5c67
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-06-08 06:14:39 -0700

    code for task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import importlib.util
-
-    spec = importlib.util.spec_from_file_location("add_0", "./add_0.py")
-    add_0 = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(add_0)
+    from add_0 import add
 
     a = 1
     b = 2
-    c = add_0.add(a, b)
+    c = add(a, b)
     print("{} + {} = {}".format(a, b, c))
