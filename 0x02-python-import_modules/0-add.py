4644d29a7882d72a58c2a34f70e4166736e61b0c
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-06-08 06:12:31 -0700

    code for task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
+    import importlib.util
 
-a = 1
-b = 2
-c = add(a, b)
-print("{} + {} = {}".format(a, b, c))
+    spec = importlib.util.spec_from_file_location("add_0", "./add_0.py")
+    add_0 = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(add_0)
+
+    a = 1
+    b = 2
+    c = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, c))
