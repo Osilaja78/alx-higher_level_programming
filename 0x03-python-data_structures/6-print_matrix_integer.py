81beffb35a2a7aa84b8b0323ee6d38a1322772fe
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-06-12 02:42:50 -0700

    code for task 6 m

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix):
-    if matrix:
+    if not matrix:
+        return None
+    else:
         for row in matrix:
             for num in row:
-                print("{:d}".format(num), end=" ")
+                print("{:d}".format(num), end=" " if col < len(row) - 1 else '')
             print()
