8a1587c002238eb8a399143d8e385ea3be2470c2
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-06-12 09:47:41 -0700

    code for task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,68 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * reverseList - reverses a linked list.
+ * @head: head of linked list
+ *
+ * Return: reversed list.
+ */
+listint_t *reverseList(listint_t *head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = head;
+	listint_t *next = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	return (prev);
+}
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome.
+ * @head: head of linked list
+ *
+ * Return: 1 if palindroem else 0.
+ */
+int is_palindrome(listint_t **head)
+{
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev = NULL;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		prev = slow;
+		slow = slow->next;
+	}
+
+	if (fast != NULL)
+		slow = slow->next;
+
+	prev->next = reverseList(slow);
+
+	listint_t *p1 = *head;
+	listint_t *p2 = prev->next;
+
+	while (p2 != NULL)
+	{
+		if (p1->n != p2->n)
+		{
+			return (0);
+		}
+		p1 = p1->next;
+		p2 = p2->next;
+	}
+
+	return (1);
+}
