e62f9bac20d5cbb75953a4840a7b75ed6de4e284
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-06-14 07:49:11 -0700

    code for task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,10 @@
 #!usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_set = set(my_list)
+    unique_set = []
+
+    for x in my_list:
+        if x not in unique_set:
+            unique_set.append(x)
 
     result = 0
     for i in unique_set:
