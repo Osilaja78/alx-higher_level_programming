08740e6007278bb21bf757f1f1f5911aac345236
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-06-14 07:43:50 -0700

    code for task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,8 @@
 #!usr/bin/python3
 def uniq_add(my_list=[]):
-    all_sum = sum(set(my_list))
-    return all_sum
+    unique_set = set(my_list)
+
+    result = 0
+    for i in unique_set:
+        result += i
+    return result
