cfc1840b04de0330b46c1995c98722607849a5c7
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-06-14 09:44:52 -0700

    code for task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,10 +3,12 @@ def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
 
-    roman_numerals = {"I": 1, "V": 5, "X": 10, "C": 100, "D": 500, "M": 1000}
+    roman_numerals = {
+            "I": 1, "V": 5, "X": 10, "C": 100, "D": 500, "M": 1000
+            }
 
-    prev_val = 0;
-    result = 0;
+    prev_val = 0
+    result = 0
     for char in reversed(roman_string):
         value = roman_numerals.get(char, 0)
         if value >= prev_val:
