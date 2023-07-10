4b52ee3caeeab42feb883fd748ddb28c562302be
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-07-10 16:24:40 -0700

    code for task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A class Square.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class square inheriting Rectangle.
+    """
+
+    def __init__(self, size):
+        """Initializes a new Square object"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square"""
+
+        return self.__size ** 2
