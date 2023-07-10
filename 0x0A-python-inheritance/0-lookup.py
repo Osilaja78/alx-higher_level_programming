efffe2aac233fb347746bf560958ddd2dcfbb0a4
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-07-10 11:44:52 -0700

    code for task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#/usr/bin/python3
+"""
+A a function that returns the list of available
+attributes and methods of an object.
+"""
+
+
+def lookup(obj):
+    """Recieves an object as argument and returns
+    a list o available attributes and methods of
+    the object"""
+
+    return (dir(obj));
