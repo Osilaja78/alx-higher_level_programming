6b2094787fdfb37be16d94811e9e84f6a7c9a07e
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-07-10 12:58:36 -0700

    code for task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    "BaseGeometry Class"
+
+    def area(self):
+        "Raises an exception"
+
+        raise Exception("area() is not implemented")
