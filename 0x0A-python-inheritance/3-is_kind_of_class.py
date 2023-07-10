66a18585609d4fe98c3e4df62789b60e3e0ddb6e
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-07-10 12:37:16 -0700

    addded code fot task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Checks if the instance of an object
+is same as passed.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Takes obj and a_class as arguments.
+
+    Checks if the instance of obj is same
+    as a_class.
+    """
+
+    return (isinstance(obj, a_class)
