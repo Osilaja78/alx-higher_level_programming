a6cdaca9864abba1e5cd3a38b1e2ec374b773111
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-07-10 15:57:56 -0700

    code for task 8 m

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,7 +10,11 @@ class Rectangle(BaseGeometry):
     "A class rectangle inheriting BaseGeometry"
 
     def __init__(self, width, height):
+        """
+        Initializes a new instance of the Rectangle class.
+        """
+
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
