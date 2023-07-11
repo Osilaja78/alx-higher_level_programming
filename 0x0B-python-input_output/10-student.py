bfd197124f1190899d7a41020231f41a15c86aeb
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-07-11 12:13:22 -0700

    code for task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A class Student.
+"""
+
+
+class Student:
+    """A class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            v_dict = {}
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    v_dict[k] = v
+            return v_dict
