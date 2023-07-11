30d81084f7052ebb1180e6c8403801621d642638
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-07-11 10:09:31 -0700

    code for task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Writes an Object to a text file, using a
+JSON representation.
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes my_obj to filename usning JSON representation"""
+
+    with open(filename, mode='w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
