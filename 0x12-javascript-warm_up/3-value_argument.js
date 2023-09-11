c5f1b49114f8194352badec03d538299b76ac42d
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-09-11 19:09:28 +0100

    fixed code for task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 //  prints a message depending of the number of arguments passed
 
-if (process.argv[2] === undefined) {
+if (!process.argv[2]) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
 }
-  
