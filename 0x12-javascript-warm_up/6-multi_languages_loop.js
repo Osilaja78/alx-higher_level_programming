81258f97cb4d361d005d0b8192842120efa41f69
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-09-11 19:41:32 +0100

    code for task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Prints 3 languages using loops and array
+
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < strings.length; i++) {
+  console.log(strings[i]);  
+}
