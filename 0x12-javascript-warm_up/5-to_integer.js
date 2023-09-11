c2c0c7023f7f364d2d1972f947e751b3b2d1de78
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-09-11 19:34:55 +0100

    code fortask 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Converts and prints first argument as a number
+
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
