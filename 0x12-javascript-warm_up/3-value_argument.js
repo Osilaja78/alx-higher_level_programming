cf3a164a21b0d4646d856f20e9ee500c8467d124
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-09-11 19:06:01 +0100

    code fortask 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//  prints a message depending of the number of arguments passed
+
+if (!process.argv[2]) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
+  
