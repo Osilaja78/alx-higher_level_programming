e0e6ed601f76128995ead94bd584ef03bb2b940b
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-09-13 09:42:22 +0100

    code for tasks 0 and 1

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// A class Rectangle
+
+module.exports = class Rectangle {};
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// A class Rectangle
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
