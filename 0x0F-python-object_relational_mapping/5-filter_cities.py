e862c346ba0e5316a4a8e007c19ad9cc668ee833
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-09-16 19:22:14 +0100

    fixed code for task 5 m

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                          passwd=password, db=database)
 
     cur = db.cursor()
-    cur.execute("SELECT cities.nameFROM cities JOIN \
+    cur.execute("SELECT cities.name FROM cities JOIN \
                  states ON cities.state_id = states.id WHERE states.name LIKE \
                  %s ORDER BY cities.id", (name,))
     cities = cur.fetchall()
