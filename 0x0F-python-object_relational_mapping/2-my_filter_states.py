2304aa147fa5e8d967ae892f318b574d3e0c9e38
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-09-16 18:21:05 +0100

    code for task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
+
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM states WHERE name LIKE '{name}' ORDER BY id ASC")
+    states = cur.fetchall()
+
+    for state in states:
+        if state[1] == name:
+            print(state)
+
+    cur.close()
+    db.close()
