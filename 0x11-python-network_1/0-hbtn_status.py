07f05c22da1fdcd7a05a644ebd727f4adeb9cf95
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-09-29 20:46:26 +0100

    code for task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This script Fetches https://alx-intranet.hbtn.io/status """
+
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+        content = r.read()
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('utf-8')}")
